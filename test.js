c77a1d0407f0110f083cd685db5e1d2c3315b432
baljitp <baljit.pal@chelsea-apps.com>
2017-03-26 19:17:46 +0100

    added test to get store data

--- test.js
+++ test.js
@@ -1,8 +1,31 @@
 const assert = require('assert');
 const checkout = require('.');
 const utils = require('./utils');
+const request = require('request');
+const rp = require('request-promise');
+const BASE_URL = 'http://localhost:3000';
 
 it('is a function', () => assert.equal(typeof checkout, 'function'));
+
+
+
+const _store = {
+    A: { price: 50, label: 'A' },
+    B: { price: 30, label: 'B' },
+    C: { price: 20, label: 'C' },
+    D: { price: 15, label: 'D' }
+};
+
+describe('init', () => {
+    it('get store', () =>{
+        return rp(`${BASE_URL}/`)
+            .then((res) => {
+                let response = JSON.parse(res);
+                assert.deepEqual(response, _store);
+            });
+    });
+});
+
 
 
 describe('utility functions', () => {
