56ff87ef87c8b3b956e3cbf4be496dbb6ff66a44
baljitp <baljit.pal@chelsea-apps.com>
2017-03-26 19:06:24 +0100

    added test with dep on utility

--- test.js
+++ test.js
@@ -1,4 +1,14 @@
 const assert = require('assert');
 const checkout = require('.');
+const utils = require('./utils');
 
 it('is a function', () => assert.equal(typeof checkout, 'function'));
+
+
+describe('utility functions', () => {
+    it('get sum from an array of numbers', () =>{
+        let data = [1, 2, 3, 4, 5];
+        let sum = utils.sumFromArray(data);
+        assert.equal(sum, 15);  
+    });
+});
