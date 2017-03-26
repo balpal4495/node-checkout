9deb7e752787f1d1d78325a9916b615ac0be3e76
baljitp <baljit.pal@chelsea-apps.com>
2017-03-26 19:48:55 +0100

    added some comments

--- index.js
+++ index.js
@@ -25,6 +25,49 @@ app.get('/calculate/:items', (req, res) => {
     res.json(total);
 });
 
+app.get('/calculateDiscounts/:items', (req, res) => {
+    let _itemString = req.params.items;
+    let _itemArray = utils.stringToArray(_itemString);
+    
+    /**
+     * This will group all the items being sent
+     */
+    let _groupedItems = 
+     _itemArray.reduce((allItems, item) => { 
+        if (item in allItems) {
+            allItems[item]++;
+        } else {
+            allItems[item] = 1;
+        }
+            return allItems;
+    }, {});
+
+
+    /**
+     * This should ideally be wrapped in a utility function
+     */
+    let discounts = [];
+     Object.keys( _groupedItems ).forEach( item => {
+         //only discount is important but the other data can be useful
+          let newObject = {item: item, occurence: _groupedItems[item], discount: null};
+          if(item === 'A' && _groupedItems[item] >= 3 ) {
+              let _discount = _groupedItems[item] / 3;
+              _discount = Math.floor(_discount);
+              _discount = _discount * 20;
+              newObject.discount = _discount;
+          }
+          if(item === 'B' && _groupedItems[item] >= 2) {
+              let _discount = _groupedItems[item] / 2;
+              _discount = Math.floor(_discount);
+              _discount = _discount * 15;
+              newObject.discount = _discount;
+          }
+          discounts.push(newObject.discount);
+      }); 
+    let total = utils.sumFromArray(discounts);
+    res.json(total);
+});
+
 const server = app.listen(3000, () => {
   const port = server.address().port;
   console.log('Example app listening at port %s', port);
